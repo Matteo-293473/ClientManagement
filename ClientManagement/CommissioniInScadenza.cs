e94a0b085d237e53bfe7368a1d1db9a2788a3fea
Matteo.exe <m.pulcinelli@campus.uniurb.it>
2021-02-22 18:32:49 +0100

    aggiornare la listview quando si preme il pulsante

--- ClientManagement/CommissioniInScadenza.cs
+++ ClientManagement/CommissioniInScadenza.cs
@@ -24,11 +24,20 @@ namespace ClientManagement
 
         private void CommissioniInScadenza_Load(object sender, EventArgs e)
         {
+            ListViewItem lst;
+            string[] arr = new string[5];
             foreach (Cliente cl in ListaClienti.clienti)
                 foreach (Commissione cm in cl.Commissioni) 
                 {
+                    arr[0] = cl.Nome;
+                    arr[1] = cl.Cognome;
+                    arr[2] = cl.Email;
+                    arr[3] = cl.Numero;
+                    arr[4] = cm.Descrizione;
+                    lst = new ListViewItem(arr);
                     //lstCommissioniScadenza.Items.Add(cm.Descrizione);
-                    lstCommissioniScadenza.Items.Add(cm);
+                    lstCommissioniScadenza.Items.Add(lst);
+                    //lstCommissioniScadenza.Items.Add(cm);
                 }
                     
 
