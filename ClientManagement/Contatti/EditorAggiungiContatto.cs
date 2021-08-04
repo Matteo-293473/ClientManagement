4e71f0c9135b9c865257ce77562fcf6560a4d7f0
Matteo-293473 <63479971+Matteo-293473@users.noreply.github.com>
2021-08-05 01:01:51 +0200

    Aggiunta ereditarietà tra EditorAggiungiContatto e EditorAggiungiCommissione

--- ClientManagement/Contatti/EditorAggiungiContatto.cs
+++ ClientManagement/Contatti/EditorAggiungiContatto.cs
@@ -9,30 +9,79 @@ namespace ClientManagement.Contatti
 {
     class EditorAggiungiContatto
     {
-        private TextBox txtNome;
-        private TextBox txtCognome;
-        private readonly ComboBox cmbEmail;
-        private TextBox txtNumeroTelefono;
-        private TextBox txtDescrizioneCommissione;
-        private DateTimePicker dtpScadenza;
+        // diamo la visibilità anche alle classi derivate
+        protected TextBox txtNome;
+        protected TextBox txtCognome;
+        protected readonly ComboBox cmbEmail;
+        protected TextBox txtNumeroTelefono;
 
         public string Nome { get => txtNome.Text; }
         public string Cognome { get => txtCognome.Text; }
         public string Email { get => cmbEmail.Text; }
         public string NumeroTelefono { get => txtNumeroTelefono.Text; }
 
-        public EditorAggiungiContatto()
+        public List<String> domini = new List<String>()
         {
+                "gmail.com",
+                "outlook.com",
+                "live.com",
+                "libero.it",
+                "alice.it.com"
+        };
 
+        //OK
+        public EditorAggiungiContatto(TextBox txtNome, TextBox txtCognome, ComboBox cmbEmail, TextBox txtNumeroTelefono)
+        {
+            this.txtNome = txtNome;
+            this.txtCognome = txtCognome;
+            this.cmbEmail = cmbEmail;
+            this.txtNumeroTelefono = txtNumeroTelefono;
+        }
+
+        //OK
+        internal virtual void ResetFields()
+        {
+            txtNome.Text = "";
+            txtCognome.Text = "";
+            cmbEmail.Text = "";
+            txtNumeroTelefono.Text = "";
 
         }
 
-        internal void AggiornaListView()
+        internal void ControllaNumeri(KeyPressEventArgs e)
         {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        internal void ControllaCaratteri(KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+
+        // OK
+        internal virtual void ControllaValiditaInput()
+        {
+            // eliminiamo degli spazi e caratteri indesiderati alla fine dei nomi
+            var nome = txtNome.Text.Split(' ');
+            var cognome = txtCognome.Text.Split(' ');
+            txtNome.Text = nome[0];
+            txtCognome.Text = cognome[0];
+
+
+            // controllo degli input
+            if (!txtNumeroTelefono.Text.All(char.IsDigit))
+                throw new Exception("Numero di telefono non valido");
+
+            if (!txtNome.Text.All(char.IsLetter))
+                throw new Exception("Nome non valido, ci sono caratteri errati");
+
+
             
-             
-           
 
         }
+
+
+
     }
 }
--- ClientManagement/Home/EditorHandlerAggiungiCommissione.cs
+++ ClientManagement/Home/EditorHandlerAggiungiCommissione.cs
@@ -8,88 +8,56 @@ using ClientManagement.Models;
 
 namespace ClientManagement
 {
-    class EditorHandlerAggiungiCommissione
+    class EditorHandlerAggiungiCommissione : EditorAggiungiContatto
     {
 
-        private TextBox txtNome;
-        private TextBox txtCognome;
-        private readonly ComboBox cmbEmail;
-        private TextBox txtNumeroTelefono;
         private TextBox txtDescrizioneCommissione;
         private DateTimePicker dtpScadenza;
 
-        public string Nome { get => txtNome.Text; }
-        public string Cognome { get => txtCognome.Text; }
-        public string Email { get => cmbEmail.Text; }
-        public string NumeroTelefono { get => txtNumeroTelefono.Text; }
+        
         public string DescrizioneCommissione { get => txtDescrizioneCommissione.Text; }
         public DateTime Scadenza { get => dtpScadenza.Value; }
 
-        public List<String> domini = new List<String>()
-        {
-                "gmail.com",
-                "outlook.com",
-                "live.com",
-                "libero.it",
-                "alice.it.com"
-        };
 
-        public EditorHandlerAggiungiCommissione(TextBox txtNome, TextBox txtCognome, ComboBox cmbEmail, TextBox txtNumeroTelefono, TextBox txtDescrizioneCommissione, DateTimePicker dtpScadenza)
+        public EditorHandlerAggiungiCommissione(TextBox txtNome, 
+            TextBox txtCognome, 
+            ComboBox cmbEmail, 
+            TextBox txtNumeroTelefono, 
+            TextBox txtDescrizioneCommissione, 
+            DateTimePicker dtpScadenza) : base(txtNome,
+            txtCognome,
+            cmbEmail,
+            txtNumeroTelefono)
         {
-            this.txtNome = txtNome;
-            this.txtCognome = txtCognome;
-            this.cmbEmail = cmbEmail;
-            this.txtNumeroTelefono = txtNumeroTelefono;
             this.txtDescrizioneCommissione = txtDescrizioneCommissione;
             this.dtpScadenza = dtpScadenza;
         }
         
-        internal void ResetFields()
+        internal override void ResetFields() 
         {
-            txtNome.Text = "";
-            txtCognome.Text = "";
-            cmbEmail.Text = "";
-            txtNumeroTelefono.Text = "";
+            // richiamo il metodo della classe padre
+            base.ResetFields();
+
+            // estendo il codice
             txtDescrizioneCommissione.Text = "";
             dtpScadenza.ResetText();
 
         }
 
-        internal void ControllaNumeri(KeyPressEventArgs e)
+
+
+        internal override void ControllaValiditaInput()
         {
-            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar); 
-        }
-
-        internal void ControllaCaratteri(KeyPressEventArgs e)
-        {
-            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar);
-        }
-
-        internal void ControllaValiditaInput()
-        {
-            // eliminiamo degli spazi e caratteri indesiderati alla fine dei nomi
-            var nome = txtNome.Text.Split(' ');
-            var cognome = txtCognome.Text.Split(' ');
-            txtNome.Text = nome[0];
-            txtCognome.Text = cognome[0];
-
-
-            // controllo degli input
-            if (!txtNumeroTelefono.Text.All(char.IsDigit))
-                throw new Exception("Numero di telefono non valido");
-
-            if (!txtNome.Text.All(char.IsLetter))
-                throw new Exception("Nome non valido, ci sono caratteri errati");
-
+            base.ControllaValiditaInput();
 
             if (!txtCognome.Text.All(char.IsLetter))
                 throw new Exception("Cognome non valido, ci sono caratteri errati");
 
-            if(dtpScadenza.Value < DateTime.Now)  // controlliamo che non sia una data passata
+            if (dtpScadenza.Value < DateTime.Now)  // controlliamo che non sia una data passata
                 throw new Exception("La data non può essere una passata");
-
-
         }
+
+
 
 
         internal void InserisciCommissioneCliente()
@@ -101,8 +69,6 @@ namespace ClientManagement
             // aggiungo commissione e cliente al managerCommissioni al cliente
             CommissionManager.AggiungiClienteCommissione(cl, cm);
         }
-
-
 
 
 
@@ -140,7 +106,7 @@ namespace ClientManagement
             // al fine di risolvere i problemi con gli omonimi, il cliente viene identificato
             // da tre campi che ne determinano una sorta di superchiave.
             
-            // popolo i campi attraverso il cliente ricavato
+            // popolo i campi attraverso il cliente trovato
             txtNome.Text = clienteFiltrato.Nome;
             txtCognome.Text = clienteFiltrato.Cognome;
             cmbEmail.Text = clienteFiltrato.Email;
