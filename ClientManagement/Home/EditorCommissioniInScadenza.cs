ea6380c82f01010a8d3dabefc970ae2596990cf0
Matteo-293473 <63479971+Matteo-293473@users.noreply.github.com>
2021-08-26 14:24:49 +0200

    modificare editorCommisisonInScadenza

--- ClientManagement/Home/EditorCommissioniInScadenza.cs
+++ ClientManagement/Home/EditorCommissioniInScadenza.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using ClientManagement.Extensions;
 using ClientManagement.Models;
@@ -27,23 +28,18 @@ namespace ClientManagement
             foreach (var i in CommissionManager.clienteCommissioni)
             {
                 List<Commissione> temp = i.Value;
-                foreach (Commissione cm in temp)
+                foreach (var cm in temp.Where(cm => cm.TaskCompletato == false && cm.Scadenza < nextSunday))
                 {
-                    if (cm.TaskCompletato == false) // se è un task completato non lo inseriamo nella lista
-                    {
-                        arr[0] = CommissionManager.clienti[i.Key].Nome;
-                        arr[1] = CommissionManager.clienti[i.Key].Cognome;
-                        arr[2] = CommissionManager.clienti[i.Key].Email;
-                        arr[3] = CommissionManager.clienti[i.Key].Numero;
-                        arr[4] = cm.Descrizione;
-                        arr[5] = cm.Scadenza.ToString().Substring(0, 10); // mostriamo solo la data
-                        arr[6] = cm.IdCommissione.ToString();
-                        lst = new ListViewItem(arr);
-
-                        //se l'item non esiste allora lo inseriamo
-                        if (cm.Scadenza < nextSunday)
-                            lstCommissioniScadenza.Items.Add(lst);
-                    }
+                    // QUESTO SI PUO' CONVERTIRE CON UNA FUNZIONE
+                    arr[0] = CommissionManager.clienti[i.Key].Nome;
+                    arr[1] = CommissionManager.clienti[i.Key].Cognome;
+                    arr[2] = CommissionManager.clienti[i.Key].Email;
+                    arr[3] = CommissionManager.clienti[i.Key].Numero;
+                    arr[4] = cm.Descrizione;
+                    arr[5] = cm.Scadenza.ToString().Substring(0, 10); // mostriamo solo la data
+                    arr[6] = cm.IdCommissione.ToString();
+                    lst = new ListViewItem(arr);
+                    lstCommissioniScadenza.Items.Add(lst);
                 }
             }
 
